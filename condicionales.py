6b5b1fe056e1b79c72ef24a395a4c7dae5d9d76e
Manuel Avalos <manuelavalosave@gmail.com>
2021-11-15 12:34:27 -0600

    Actualizacion de comentarios

--- condicionales.py
+++ condicionales.py
@@ -1,6 +1,10 @@
+#Obtenemos la edad del usuario final
 edad = int(input("Escribe tu edad: "))
+#se realiza la condicion para validar el numero ingresado
 if edad > 17:
+    #si es verdadero entonce mostrara este mensaje
     print("Eres mayor de edad")
 else:
+    #sino se cumple se muestra este mensaje
     print("Eres menor de edad")
 
